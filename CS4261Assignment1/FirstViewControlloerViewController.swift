3723c6cd0f0792607651f6da5b1d60939c030f1c
Reagan Hanna <rhanna@gatech.edu>
2019-09-03 11:58:28 -0400

    Reagan Hanna 3
    Added Second view screen, attempted to add navigation control functionality between login screen and home page

---
+++ CS4261Assignment1/FirstViewControlloerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewControlloerViewController.swift
+//  CS4261Assignment1
+//
+//  Created by Reagan Hanna on 9/3/19.
+//  Copyright © 2019 Brooke Brennan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewControlloerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
