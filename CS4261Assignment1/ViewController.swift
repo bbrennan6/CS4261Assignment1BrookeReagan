a61ffbdae335b355579c495fc0607c2412dc9efb
Brooke Brennan <BBBrennan3@gmail.com>
2019-09-02 21:10:36 -0400

    Initial Commit

---
+++ CS4261Assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS4261Assignment1
+//
+//  Created by Brooke Brennan on 9/2/19.
+//  Copyright © 2019 Brooke Brennan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CS4261Assignment1Tests/CS4261Assignment1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CS4261Assignment1Tests.swift
+//  CS4261Assignment1Tests
+//
+//  Created by Brooke Brennan on 9/2/19.
+//  Copyright © 2019 Brooke Brennan. All rights reserved.
+//
+
+import XCTest
+@testable import CS4261Assignment1
+
+class CS4261Assignment1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
